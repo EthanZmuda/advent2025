cb3077818fbcaae02a0bc706535d83058d639b13
Ethan Zmuda <ethan.zmuda@gmail.com>
2025-12-01 23:23:29 -0600

    day 1 descriptive types

--- 01/main.hs
+++ 01/main.hs
@@ -2,26 +2,31 @@ module Main where
 
 import System.IO.Unsafe (unsafePerformIO)
 
-part1 :: String -> Int
+type TextInput = String
+type Instruction = String
+type Dial = Int
+type SeenZeros = Int
+
+part1 :: TextInput -> SeenZeros
 part1 contents = snd (foldl accFunc (50, 0) $ lines contents) where
 
-  accFunc :: (Int, Int) -> String -> (Int, Int)
+  accFunc :: (Dial, SeenZeros) -> Instruction -> (Dial, SeenZeros)
   accFunc acc ('L':xs) = combine ((fst acc - read xs) `mod` 100) (snd acc)
   accFunc acc ('R':xs) = combine ((fst acc + read xs) `mod` 100) (snd acc)
 
-  combine :: Int -> Int -> (Int, Int)
+  combine :: Dial -> SeenZeros -> (Dial, SeenZeros)
   combine total inc = (total, inc + fromEnum (total == 0))
 
-part2 :: String -> Int
+part2 :: TextInput -> SeenZeros
 part2 contents = snd (foldl accFunc (50, 0) $ lines contents) where
 
-  accFunc :: (Int, Int) -> String -> (Int, Int)
+  accFunc :: (Dial, SeenZeros) -> Instruction -> (Dial, SeenZeros)
   accFunc acc step =
     case step of
       'L':xs -> let total = fst acc - read xs in (total `mod` 100, snd acc + handleIncrementL total (fst acc))
       'R':xs -> let total = fst acc + read xs in (total `mod` 100, snd acc + (total `div` 100))
   
-  handleIncrementL :: Int -> Int -> Int
+  handleIncrementL :: Dial -> Dial -> SeenZeros
   handleIncrementL t prev
     | t <= 0    = fromEnum (prev /= 0) + ((-t) `div` 100)
     | otherwise = 0
